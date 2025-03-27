faa7a75e0b9624295a5bcb1dac6040de9cead346
Ryan Prendergast <rprendergast1121@gmail.com>
2025-03-27 11:10:31 -0700

    Use the correct audience

--- src/config/auth0.ts
+++ src/config/auth0.ts
@@ -2,6 +2,6 @@ export const auth0Config = {
   domain: "dev-u0ert1rxhkdmhwy8.us.auth0.com",
   clientId: "5UHbVoaoZlx8uvDqQjUiYPtiE3AJv5yc",
   redirectUri: window.location.origin + "/login",
-  audience: `https://zenobiapay.com`,
+  audience: `https://dashboard.zenobiapay.com`,
   scope: "openid profile email",
 }
