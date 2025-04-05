dbdcc873f1d5b16e98eb610a104a7ecd6939cb80
Theodore Li <teddy@zenobiapay.com>
2025-04-04 19:42:11 -0700

    Switch auth0 to beta client

--- src/config/auth0.ts
+++ src/config/auth0.ts
@@ -1,8 +1,8 @@
 export const auth0Config = {
   // domain: "accounts.zenobiapay.com",
   // clientId: "5UHbVoaoZlx8uvDqQjUiYPtiE3AJv5yc",
-  domain: "dev-iols5y7cp32hlyr1.us.auth0.com",
-  clientId: "BqmnZwd83LDvexDry1uqOWfX8oQIIg2o",
+  domain: "zenobia-beta.us.auth0.com",
+  clientId: "gRg4qNX32qmtoLOUH8wmojid0So9MkqB",
   redirectUri: window.location.origin + "/login",
   audience: `https://dashboard.zenobiapay.com`,
   scope: "openid profile email",
