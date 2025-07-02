3fdf7c58ebbcaa7da2017fa3c8ba963c2368aca6
Ryan Prendergast <rprendergast1121@gmail.com>
2025-07-01 17:58:33 -0700

    Fix landing page deployments

--- apps/landing-page/app.config.ts
+++ apps/landing-page/app.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    preset: "cloudflare",
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
